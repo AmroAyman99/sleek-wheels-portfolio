4a907ce3808ae6dc83dd8b02ac1d95d5cf18a569
fathy10010 <fathymohamed01067@gmail.com>
2025-05-15 03:42:35 +0300

    refactor:adjusting facebook url

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
               Premium car service providing luxury transportation for any occasion. Your comfort and satisfaction are our top priorities.
             </p>
             <div className="flex space-x-4 mt-6">
-              <a href="#" className="h-10 w-10 rounded-full bg-white/10 flex items-center justify-center hover:bg-luxury-primary hover:text-black transition-colors">
+              <a href="https://web.facebook.com/tota.speed" className="h-10 w-10 rounded-full bg-white/10 flex items-center justify-center hover:bg-luxury-primary hover:text-black transition-colors">
                 <span className="sr-only">Facebook</span>
                 <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                   <path fillRule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clipRule="evenodd" />
